356f8c589b603284c861d109d7721551efcf977b
Tanmay Aswale <103102215+AswaleTanmay@users.noreply.github.com>
2022-11-29 10:03:06 +0530

    Day 10
    Get a number from user for which you need to fin the factorial, then calculate the factorial and give it as the output

---
+++ 10th Day.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int i,n;
+cout<<"Enter Number";
+cin>>n;
+for(i=1;i<=n;i++)
+{
+if(n<=i)
+{
+cout<<n*1;
+}
+else
+{
+cout<<"Invali";
+}
+}
+}
