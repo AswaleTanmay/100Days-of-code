1cb959a49f53fa8e9368f19ecd34dec46c63dc8d
Tanmay Aswale <103102215+AswaleTanmay@users.noreply.github.com>
2022-12-30 21:39:04 +0530

    Day37
    Get a string as the input from the user and find the frequency of characters in the string.

---
+++ 37th Day.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+ char str[100];
+ int i;
+ int frequency[256] = {0};
+ cout<<"Enter a string: ";
+ gets(str);
+ for(i = 0; str[i] != '\0'; i++)
+ {
+ frequency[str[i]]++;
+ }
+ for(i = 0; i < 256; i++)
+ {
+ if(frequency[i] != 0)
+ {
+ printf("The frequency of %c is %d\n", i, frequency[i]);
+ }
+ }
+ return 0;
+}
